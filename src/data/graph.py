Fix motion features computation, clean-up unused motion features and attributes

src/data/graph.py
@@ -2,7 +2,7 @@ import torch
 from torch_geometric.data import Data
 import numpy as np
 from src.utils.graph_utils import find_graph_time_valid_edges, prune_edges, compute_edge_features, assign_edge_labels, make_symmetric_edges, assign_node_ids
-from src.utils.motion_utils import compute_vel_feats, torch_get_motion_feats
+from src.utils.motion_utils import  torch_get_motion_feats
 from torch_scatter import scatter_mean, scatter_max, scatter_min, scatter_add
 from torch.nn.utils.rnn import pad_sequence
 import torch.nn.functional as F
@@ -203,14 +203,10 @@ class HierarchicalGraph(Data):
 
         return x_track, x_track_length
 
-    def get_motion_features(self, max_length, interpolate, parametrization):
+    def get_motion_features(self, max_length, interpolate):
         """ Wrapper function around the actual numba code that gathers the trajectory information
         of each node in the current graph
         """
-        gpu_device = self.x_frame.device
-
-        # Prepare inputs
-        
         motion_feats = torch_get_motion_feats(map_from_init=self.map_from_init,
                                               x_frame_=self.x_frame,
                                               x_center=self.x_center,
@@ -220,24 +216,11 @@ class HierarchicalGraph(Data):
         
         
         have_non_singleton = not motion_feats['x_ignore_traj'].all()
-        if have_non_singleton:
-            motion_feats['x_fwrd_missing'] = None # Dummy assignments, theses are not used
-            motion_feats['h_fwrd_vel'] = None # Dummy assignments, theses are not used
-            motion_feats['x_bwrd_missing'] = None # Dummy assignments, theses are not used
-            motion_feats['h_bwrd_vel'] = None # Dummy assignments, theses are not used
-
-        else:
-            motion_feats = {'x_fwrd_rel_vel': torch.zeros((0, 4), device=gpu_device),
-                            'x_bwrd_rel_vel': torch.zeros((0, 4), device=gpu_device),
-                            'x_fwrd_motion': torch.zeros((0, max_length, 4), device=gpu_device),
-                            'x_bwrd_motion': torch.zeros((0, max_length, 4), device=gpu_device),
-                            'x_fwrd_length': torch.zeros((0,),dtype=torch.int, device=gpu_device),
-                            'x_bwrd_length': torch.zeros((0,),dtype=torch.int, device=gpu_device),
-                            'x_ignore_traj': torch.as_tensor(motion_feats['x_ignore_traj']).to(gpu_device),
-                            'x_fwrd_missing': torch.zeros((0, max_length), device=gpu_device),
-                            'x_bwrd_missing': torch.zeros((0, max_length), device=gpu_device),
-                            'h_bwrd_vel': torch.zeros((0, 4), device=gpu_device),
-                            'h_fwrd_vel': torch.zeros((0, 4), device=gpu_device),
+        if not have_non_singleton:
+            device = self.x_frame.device
+            motion_feats = {'x_fwrd_motion': torch.zeros((0, max_length, 4), device=device),
+                            'x_bwrd_motion': torch.zeros((0, max_length, 4), device=device),
+                            'x_ignore_traj': motion_feats['x_ignore_traj']
                             }
 
         return motion_feats
@@ -259,8 +242,7 @@ class HierarchicalGraph(Data):
         
         if self.curr_depth >0 and config.do_motion:
             motion_features = self.get_motion_features(max_length = config.motion_max_length[self.curr_depth - 1],
-                                                       interpolate =config.interpolate_motion,
-                                                       parametrization=config.motion_parametrization)
+                                                       interpolate =config.interpolate_motion)
             fwrd_vel = torch.zeros(((~motion_features['x_ignore_traj']).sum(), 4), device = x_node.device, dtype=torch.float)
             bwrd_vel = fwrd_vel.clone()
                 
@@ -306,9 +288,6 @@ class HierarchicalGraph(Data):
         # Edge features
         motion_feats = {'motion_giou': curr_graph.pruning_score}
         
-        #edge_feats_to_use_ = ['secs_time_dists', 'norm_feet_x_dists', 'norm_feet_y_dists',
-        #                      'bb_height_dists', 'bb_width_dists', 'emb_dists']
-
         edge_feats_to_use_ = ['secs_time_dists']
         if config.mpn_use_pos_edge[self.curr_depth]:
             edge_feats_to_use_ += ['norm_feet_x_dists', 'norm_feet_y_dists', 'bb_height_dists', 'bb_width_dists']
@@ -318,15 +297,7 @@ class HierarchicalGraph(Data):
 
         if config.mpn_use_motion[self.curr_depth]:
             edge_feats_to_use_ += ['motion_giou']
-
-        if config.mpn_use_baseline_feats[self.curr_depth]:
-            edge_feats_to_use_ += ['baseline_clusters_cosine', 'baseline_clusters_l2']
                         
-        if config.mpn_use_vel_feats[self.curr_depth]:
-            vel_feats = compute_vel_feats(fwrd_vel_=curr_graph.fwrd_vel, bwrd_vel_=curr_graph.bwrd_vel, batch=curr_graph, edge_index=edge_ixs)            
-            edge_feats_to_use_ += list(vel_feats.keys())
-            motion_feats.update(vel_feats)
-
         reid_sim_fn = F.pairwise_distance if config.edge_sim_fn == 'l2' else lambda x, y: 2 - F.cosine_similarity(x, y)
 
         edge_features = compute_edge_features(edge_ixs=edge_ixs, node_frames=x_frame, node_bboxes=x_bbox,

src/models/hiclnet.py
@@ -6,7 +6,7 @@ class HICLNet(nn.Module):
     """
     Hierarchical network that contains all layers
     """
-    def __init__(self, submodel_type, submodel_params, hicl_depth, use_bline_feats, use_motion, use_vel_feats, use_reid_edge, use_pos_edge,
+    def __init__(self, submodel_type, submodel_params, hicl_depth, use_motion, use_reid_edge, use_pos_edge,
                  share_weights, edge_level_embed, node_level_embed):
         """
         :param model_type: Network to use at each layer
@@ -15,7 +15,7 @@ class HICLNet(nn.Module):
         """
         super(HICLNet, self).__init__()
         
-        for per_layer_params in (use_bline_feats, use_motion, use_vel_feats, use_reid_edge, use_pos_edge):
+        for per_layer_params in (use_motion, use_reid_edge, use_pos_edge):
             assert hicl_depth == len(per_layer_params), f"{hicl_depth }, {per_layer_params}"
 
         assert share_weights in ('none', 'all_but_first', 'all') 
@@ -26,8 +26,8 @@ class HICLNet(nn.Module):
         layer_idxs = _SHARE_WEIGHTS_IDXS[share_weights]
 
         # All hierarchical layers are contained in a nn.ModuleList
-        layers = [submodel_type(submodel_params, bline_feats=bline_feats, motion=motion, vel_feats=vel_feats, pos_feats=pos_feats, reid=reid) 
-                  for bline_feats, motion, vel_feats, pos_feats, reid in zip(use_bline_feats, use_motion, use_vel_feats, use_pos_edge, use_reid_edge)]
+        layers = [submodel_type(submodel_params, motion=motion, pos_feats=pos_feats, reid=reid) 
+                  for motion, pos_feats, reid in zip(use_motion, use_pos_edge, use_reid_edge)]
 
         self.layers = nn.ModuleList([layers[idx] for idx in layer_idxs])
 

src/models/motion/linear.py
@@ -4,30 +4,21 @@ class LinearMotionModel:
     def __init__(self):
         pass
 
-    def __call__(self, x_motion, x_last_pos, pred_length, x_last_rel, h_vel, parametrization, linear_center_only, **kwargs):
+    def __call__(self, x_motion, x_last_pos, pred_length, linear_center_only, **kwargs):
         """Estimate the velocity from the given trajectory (`x_motion`), and forward propagate it for 
            `pred_length` steps, starting from `x_last_pos`
         """
         num_nodes = x_motion.shape[0]
         if num_nodes > 0:
             # Velocity estimation
-            if parametrization == 'abs':
-                vels = x_motion[:, 1:] - x_motion[:, :-1]
-            
-            else:
-                vels = x_motion.clone()
-
+            vels = x_motion[:, 1:] - x_motion[:, :-1]
             num_estimates = vels.shape[1] -  torch.isnan(vels).sum(1)
             assert ((num_estimates.min(-1)[0] == num_estimates.max(-1)[0])).all(), "Some coordinates are Nans, some others are not?"
             num_estimates = num_estimates.min(-1)[0]
             
             vels = torch.nan_to_num(vels, nan = 0.0)
             estimate_vel = vels.sum(dim = 1) / num_estimates[:, None]
-            estimate_vel[num_estimates == 0] = x_last_rel[num_estimates == 0]
             assert not torch.isnan(estimate_vel).any()
-
-            if parametrization == 'offsets_abs_scale':
-                estimate_vel[..., -1] = h_vel
 
             if linear_center_only:
                 estimate_vel[..., 2:]=0

src/models/mpntrack.py
@@ -232,7 +232,7 @@ class MOTMPNet(nn.Module):
 
     This class was initially based on: https://github.com/deepmind/graph_nets tensorflow implementation.
     """
-    def __init__(self, model_params, bb_encoder = None, bline_feats=None, motion=None, vel_feats=None, pos_feats=None, reid=None):
+    def __init__(self, model_params, bb_encoder = None, motion=None, pos_feats=None, reid=None):
         """
         Defines all components of the model
         Args:
@@ -245,16 +245,10 @@ class MOTMPNet(nn.Module):
         self.model_params = model_params
 
         # Define Encoder and Classifier Networks
-        encoder_feats_dict = deepcopy(model_params['encoder_feats_dict'])
-        if bline_feats:
-            encoder_feats_dict['edge_in_dim'] += 2
-            
+        encoder_feats_dict = deepcopy(model_params['encoder_feats_dict'])            
         if motion:
             encoder_feats_dict['edge_in_dim'] += 1
-        
-        if vel_feats:
-            encoder_feats_dict['edge_in_dim'] += 4
-        
+                
         if not reid:
             encoder_feats_dict['edge_in_dim'] -= 1
 

src/utils/motion_utils.py
@@ -1,150 +1,8 @@
 import numpy as np
 import torch
 import torch.nn.functional as F
-from numba import jit
 from torch_scatter import scatter_min, scatter_max, scatter_sum
 
-@jit(nopython=True)
-def _get_motion_features(map_from_init, x_frame, x_center, x_bbox, interpolate, max_length=5):
-
-    # Numba requires specifying list types like this
-    x_fwrd_motion = [np.empty((max_length, 4), np.float32) for _ in range(0)] 
-    x_bwrd_motion = [np.empty((max_length, 4), np.float32) for _ in range(0)] 
-    
-    x_fwrd_rel_vel = [np.empty((4,), np.float32) for _ in range(0)] 
-    x_bwrd_rel_vel = [np.empty((4,), np.float32) for _ in range(0)] 
-
-    x_fwrd_length = [int(_) for _ in range(0)]
-    x_bwrd_length = [int(_) for _ in range(0)]
-
-    x_ignore_traj = [np.array(False) for _ in range(0)]
-
-    x_fwrd_missing = [np.empty((max_length,), np.int32) for _ in range(0)] # Bools cause TypeErrors
-    x_bwrd_missing = [np.empty((max_length,), np.int32) for _ in range(0)] # Bools cause TypeErrors
-
-    assert max_length >1
-
-    for cluster_ix in range(map_from_init.max() + 1):
-        cluster_mask = map_from_init == cluster_ix
-        #has_traj = torch.sum(cluster_mask) > 1:
-        if np.sum(cluster_mask) > 1:
-            frames = x_frame[cluster_mask].reshape(-1)
-            #break
-
-            whs = x_bbox[cluster_mask][:, 2:]
-            centers = x_center[cluster_mask]
-            
-            # Sort elements according to time
-            #frames_sorted, ix_sort = torch.sort(frames)
-            #assert torch.all(frames == frames_sorted), 'Frames are not sorted'
-
-            # Get forward motion features
-            #fwrd_motion = np.full((MAX_LENGTH, 4), fill_value = np.nan, dtype = float)        
-            fwrd_motion = np.empty((max_length, 4), np.float32)        
-            fwrd_motion[...] = np.nan
-            fwrd_mask = (frames.max()- frames)< max_length
-            fwrd_frames = frames[fwrd_mask]
-            #print(fwrd_frames - fwrd_frames.min())
-            idx = (fwrd_frames - fwrd_frames.min()).reshape(-1)
-            fwrd_motion[idx] = np.concatenate((centers[fwrd_mask], whs[fwrd_mask]), axis = 1)
-            fwrd_length = idx.max() - idx.min() + 1
-
-            if len(idx) >=2:
-                #assert (torch.sort(idx)[0] == idx).all()
-                x_fwrd_rel = (fwrd_motion[idx[-1]] -  fwrd_motion[idx[-2]]) / (idx[-1] - idx[-2])
-            
-            else:
-                # TODO: This should be done separately for fwrd and bwrd
-                x_ignore_traj.append(np.array(True))
-                continue
-
-            #fwrd_missing = (fwrd_motion!=fwrd_motion).sum(-1) >0
-            fwrd_missing = (np.isnan(fwrd_motion).sum(-1) >0).astype(np.int32)
-            nans = fwrd_missing[:fwrd_length] >0
-            present_idxs = (~nans).nonzero()[0]
-            miss_idxs = nans.nonzero()[0]
-            
-            if interpolate:
-                for i in range(fwrd_motion.shape[1]):
-                    fwrd_motion[:fwrd_length, i][nans]= np.interp(miss_idxs, present_idxs, fwrd_motion[:fwrd_length, i][~nans])
-        
-                assert np.isnan(fwrd_motion[:fwrd_length]).sum() == 0
-
-            # Get backward motion features
-            #bwrd_motion = np.full((MAX_LENGTH, 4), fill_value = np.nan, dtype = float)        
-            bwrd_motion = np.empty((max_length, 4), np.float32)        
-            bwrd_motion[...] = np.nan
-
-            bwrd_mask = (frames-frames.min())< max_length
-            bwrd_frames = frames[bwrd_mask]
-            idx = (bwrd_frames - bwrd_frames.min()).reshape(-1)
-            idx = idx.max() - idx # Invert index
-            bwrd_motion[idx] = np.concatenate((centers[bwrd_mask], whs[bwrd_mask]), axis = 1)
-            bwrd_length = idx.max() - idx.min() + 1
-            last_bwrd_pos = bwrd_motion[idx.max()]
-                        
-            if len(idx) >=2:
-                #assert (torch.sort(idx)[0] == idx).all()
-                x_bwrd_rel = (bwrd_motion[idx[0]] -  bwrd_motion[idx[1]]) / (idx[0] - idx[1])
-            
-            else:
-                x_ignore_traj.append(np.array(True))
-                continue
-            
-            
-            bwrd_missing = (np.isnan(bwrd_motion).sum(-1) >0).astype(np.int32)
-            nans = bwrd_missing[:bwrd_length] >0
-            present_idxs = (~nans).nonzero()[0]
-            miss_idxs = nans.nonzero()[0]
-            
-            if interpolate:
-                for i in range(bwrd_motion.shape[1]):
-                    bwrd_motion[:bwrd_length, i][nans]= np.interp(miss_idxs, present_idxs, bwrd_motion[:bwrd_length, i][~nans])
-
-        
-                assert np.isnan(bwrd_motion[:bwrd_length]).sum() == 0
-            x_ignore_traj.append(np.array(False))
-
-            # Append everything
-            x_fwrd_motion.append(fwrd_motion)
-            x_bwrd_motion.append(bwrd_motion)
-            
-            x_fwrd_rel_vel.append(x_fwrd_rel)
-            x_bwrd_rel_vel.append(x_bwrd_rel)
-
-            x_fwrd_length.append(fwrd_length)
-            x_bwrd_length.append(bwrd_length)
-
-            x_fwrd_missing.append(fwrd_missing)
-            x_bwrd_missing.append(bwrd_missing)
-        else:
-            x_ignore_traj.append(np.array(True))
-
-    return x_fwrd_rel_vel, x_bwrd_rel_vel, x_fwrd_motion, x_fwrd_length, x_bwrd_motion, x_bwrd_length, x_ignore_traj, x_fwrd_missing, x_bwrd_missing
-
-def reparametrize_motion(x_motion, x_rel_vel, x_length, x_missing, parametrization):
-    assert parametrization in ('abs', 'offsets', 'offsets_abs_scale')
-    rel_pos = x_motion[:, 1:] - x_motion[:, :-1]
-    h_vel = rel_pos[..., -1]
-    avg_h_vel = torch.nan_to_num(h_vel, 0).sum(dim=1) / (x_length - 1)
-
-    if parametrization == 'abs':
-        return x_motion, x_length, x_missing, avg_h_vel
-
-    assert (torch.abs(get_last_pos(rel_pos, x_length - 1) - x_rel_vel)<1e-3).all(), torch.where((get_last_pos(rel_pos, x_length - 1) != x_rel_vel))
-    assert (torch.isnan(rel_pos[..., -1]) == torch.isnan(x_motion[..., 1:, -1])).all()
-
-    if parametrization == 'offsets_abs_scale':
-        rel_pos[..., -1] = x_motion[..., 1:, -1]
-
-    return rel_pos, x_length - 1, x_missing[:, 1:], avg_h_vel
-
-def get_last_pos(x_motion, x_length):
-    assert x_motion.dim() == 3, x_motion.shape
-    assert x_length.dim() == 1, x_length.shape
-    assert x_motion.shape[0] == x_length.shape[0], f"{x_length.shape}, {x_motion.shape}"
-    last_pos = x_motion.gather(1, x_length[:,None, None].expand(-1, -1, 4) - 1)
-    return last_pos[:, 0]
 
 def masked2original(tensor, num_nodes, nodes_mask, fill_value=0):
     assert nodes_mask.shape[0] == num_nodes
@@ -154,47 +12,6 @@ def masked2original(tensor, num_nodes, nodes_mask, fill_value=0):
     full_tensor[nodes_mask] = tensor
     return full_tensor
 
-def compute_vel_feats(fwrd_vel_, bwrd_vel_, batch, edge_index):
-    """Compute several velocity-based 'motion continuity' measures based on two track's estimated
-    forward and backward velocities"""
-
-    num_nodes = batch.num_nodes
-    fwrd_vel = masked2original(fwrd_vel_, num_nodes, ~batch.x_ignore_traj, np.nan)
-    bwrd_vel = masked2original(bwrd_vel_, num_nodes, ~batch.x_ignore_traj, np.nan)
-    row, col = edge_index
-    assert (row < col).all()
-
-    vel_angle = F.cosine_similarity(fwrd_vel[row], -bwrd_vel[col])
-    vel_angle = torch.nan_to_num(vel_angle, 0)
-
-    fwrd_norm = fwrd_vel.norm(2, dim=1)
-    bwrd_norm = fwrd_vel.norm(2, dim=1)
-    norm_sim = F.l1_loss(fwrd_norm[row], bwrd_norm[col], reduction='none')
-    norm_sim = torch.nan_to_num(norm_sim, -1)
-
-    t_diff = batch.x_frame_start[col] - batch.x_frame_end[row]
-    assert (t_diff>0).all()
-    interp_vel = (batch.x_center_start[col] - batch.x_center_end[row]) / t_diff
-    interp_vel_norm = interp_vel.norm(2, dim=1)
-    interp_vel[..., 2:] = 0
-    # TODO: should parametrize if doing w/h vel prediction!!!!
-
-    interp_angle_fwrd = F.cosine_similarity(interp_vel, fwrd_vel[row]) 
-    interp_angle_bwrd = F.cosine_similarity(interp_vel, -bwrd_vel[col]) 
-    interp_angle = (interp_angle_bwrd + interp_angle_fwrd) / 2
-    interp_angle = torch.nan_to_num(interp_angle, -1)
-
-    interp_norm_fwrd = F.l1_loss(fwrd_norm[row], interp_vel_norm, reduction='none')
-    interp_norm_bwrd = F.l1_loss(bwrd_norm[col], interp_vel_norm, reduction='none')
-    
-    interp_norm = torch.nan_to_num(0.5*(interp_norm_fwrd + interp_norm_bwrd), -1)
-
-    return {'interp_angle': interp_angle, 
-            'interp_norm': interp_norm,
-            #'interp_l1': interp_l1,
-            'norm_sim': norm_sim,
-            'vel_angle': vel_angle
-            }
 def ccwh2tlbr(boxes):
     """
     Change box parametrization from (center_x, center_y, width, height) to (top_left_x, top_left_y, bottom_right_x, bottom_right_y)
@@ -291,12 +108,11 @@ def torch_get_motion_feats(map_from_init, x_frame_, x_center, x_bbox, max_length
     # Strategy: map frames from their value to positions within the trajectory
     # Then directly index an array with shape (num_ids, max_trajectory_length) using those
     num_ids = map_from_init.max() + 1
-    count_per_id = scatter_sum(torch.ones_like(x_frame), index=map_from_init, dim=0, dim_size = num_ids)
-    keep_traj = count_per_id>1
 
     # Forward x_motion
     device = box_coords.device
     x_fwrd_bwrd_motion = []
+    keep_traj = torch.ones(num_ids, device=device, dtype=torch.bool)
     for mode in ['forward', 'backward']:
         scatter_fn = scatter_max if mode == 'forward' else scatter_min
         last_frame, _ = scatter_fn(x_frame, index=map_from_init, dim=0, dim_size = num_ids)
@@ -314,7 +130,10 @@ def torch_get_motion_feats(map_from_init, x_frame_, x_center, x_bbox, max_length
         # Assign Values
         x_motion_ = torch.full((num_ids, max_length, 4), fill_value = np.nan, device=device)
         x_motion_[map_from_init[mask], norm_frame[mask]] = box_coords[mask] 
-        x_motion_ = x_motion_[keep_traj]
+        
+        # Ignore trajectories that do not have more than one past box
+        count_per_id = scatter_sum(mask.int(), index=map_from_init, dim=0, dim_size = num_ids)
+        keep_traj = keep_traj & (count_per_id>1)
 
         if interpolate: # tricky to vectorize
             # Determine which values need to be interpolated (dont extrapolate starts of trajectories)
@@ -343,14 +162,7 @@ def torch_get_motion_feats(map_from_init, x_frame_, x_center, x_bbox, max_length
 
         #feat_names = ['x_fwrd_rel_vel', 'x_bwrd_rel_vel', 'x_fwrd_motion', 'x_fwrd_length', 'x_bwrd_motion', 'x_bwrd_length', 'x_ignore_traj', 'x_fwrd_missing', 'x_bwrd_missing']
 
-    return dict(x_fwrd_motion=x_fwrd_bwrd_motion[0],
-                x_bwrd_motion=x_fwrd_bwrd_motion[1],
-                x_ignore_traj = ~keep_traj,
-
-
-                # dummy assignments to not break things, but they should not be used
-                x_fwrd_rel_vel=torch.empty_like(x_fwrd_bwrd_motion[1][:, 0]),
-                x_bwrd_rel_vel=torch.empty_like(x_fwrd_bwrd_motion[1][:, 0]),
-                x_bwrd_length = torch.ones_like(x_fwrd_bwrd_motion[1][:, 0], dtype=torch.int),
-                x_fwrd_length = torch.ones_like(x_fwrd_bwrd_motion[1][:, 0], dtype=torch.int)
+    return dict(x_fwrd_motion=x_fwrd_bwrd_motion[0][keep_traj],
+                x_bwrd_motion=x_fwrd_bwrd_motion[1][keep_traj],
+                x_ignore_traj = ~keep_traj
                 )